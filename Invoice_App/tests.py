b66c59a1228b7958b015a593a7bf0ca3a473059c
TajQureshi <137213646+TajQureshi@users.noreply.github.com>
2023-09-25 13:27:39 +0530

    Update tests.py

--- Invoice_App/tests.py
+++ Invoice_App/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from .models import Invoice
+
 
 # Create your tests here.
+
+class InvoiceAPITestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.invoice_data = {
+            'date': '2023-09-23',
+            'customer_name': 'Test Customer',
+        }
+
+    def test_create_invoice(self):
+        response = self.client.post('/invoices/', self.invoice_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Invoice.objects.count(), 1)
+        self.assertEqual(Invoice.objects.get().customer_name, 'Test Customer')
